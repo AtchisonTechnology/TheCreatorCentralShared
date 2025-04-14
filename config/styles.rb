d55e19a45010054824f8e0b706952ebcccb7853d
Lee Atchison <lee@leeatchison.com>
2025-04-14 09:59:29 -0700

    Improved configuration.

---
+++ config/styles.rb
@@ -0,0 +1,7 @@
+BaseStyles = {
+  this_is_a_test: "test",
+  or_this: [
+    {a: "test"},
+    {b: "test"}
+  ],
+}
--- shared/shared.rb
+++ shared/shared.rb
@@ -1,2 +1,14 @@
 require_rel './shared/'
 #require_rel './renderers/'
+
+require_rel '../config/styles.rb'
+
+StylesConfig = BaseStyles.merge(YAML::load(File.read("#{TheCreatorCentralShared}/config/styles.yml"))).deep_symbolize_keys
+SocialConfig = YAML::load(File.read("#{TheCreatorCentralShared}/config/social.yml")).deep_symbolize_keys
+SubscriptionPackages = YAML::load(File.read("#{TheCreatorCentralShared}/config/subscription_packages.yml")).deep_symbolize_keys
+WidgetsAndLayoutConfig = YAML::load(File.read("#{TheCreatorCentralShared}/config/widgets_and_layouts.yml")).deep_symbolize_keys
+WidgetInfo = WidgetsAndLayoutConfig[:widgets]
+PageLayoutInfo = WidgetsAndLayoutConfig[:page_layouts]
+SharedAppConfig = {}.merge(YAML::load(File.read("#{TheCreatorCentralShared}/config/shared_configuration.yml"))["shared"]||{bad: :stuff})
+                    .merge(YAML::load(File.read("#{TheCreatorCentralShared}/config/shared_configuration.yml"))[CurrentEnvironment]||{bad: :stuff})
+                    .deep_symbolize_keys
