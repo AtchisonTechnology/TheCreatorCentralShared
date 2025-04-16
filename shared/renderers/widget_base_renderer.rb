af31d6a780df1448f1438ae56c9041f274052706
Lee Atchison <lee@leeatchison.com>
2025-04-15 20:42:28 -0700

    Added styles to base widgets.

--- shared/renderers/widget_base_renderer.rb
+++ shared/renderers/widget_base_renderer.rb
@@ -38,10 +38,17 @@ class WidgetBaseRenderer < BaseRenderer
     retHash["padding-left"] = widget.styles["padding_left"]
     retHash["border"] = "#{widget.styles["border_size"]} solid #{widget.styles["border_color"]}" if widget.styles["border_size"].present?
     retHash["fixed-position"] = widget.styles["fixed_position"] if widget.styles["fixed_position"].present?
-    # LEELEE: shadow
-    # LEELEE: rounded
+    if widget.styles["shadow"].present?
+      if  widget.styles["shadow"] == "basic"
+        retHash["box-shadow"] = "10px 5px 5px #ddd" # TODO: More options here?
+      end
+    end
+    if widget.styles["rounded"].present?
+      if  widget.styles["rounded"] == "basic"
+        retHash["border-radius"] = "20px" # TODO: More options here?
+        retHash["padding"] = "15px" # TODO: More options here?
+      end
+    end
     hashToStyleString retHash
   end
-
 end
-
