9c07b793b7e3f031fbc510f3bf2cf6130d643138
Lee Atchison <lee@leeatchison.com>
2025-04-23 21:30:04 -0700

    Fly-in support.

--- shared/renderers/widget_base_renderer.rb
+++ shared/renderers/widget_base_renderer.rb
@@ -14,6 +14,7 @@ class WidgetBaseRenderer < BaseRenderer
     ret = {}
     ret["tcc_widget_outer"] = tcc_widget_outer
     ret["tcc_widget"] = tcc_widget
+    ret["tcc_widget_classes"] = tcc_widget_classes
     self.render_inherited ret
     ret
   end
@@ -32,15 +33,15 @@ class WidgetBaseRenderer < BaseRenderer
   def tcc_widget
     retHash = {}
     retHash["width"] = widget.styles["width"] if widget.styles["width"].present?
-    retHash["color"] = widget.styles["text_color"]
-    retHash["margin-top"] = widget.styles["margin_top"]
-    retHash["margin-right"] = widget.styles["margin_right"]
-    retHash["margin-bottom"] = widget.styles["margin_bottom"]
-    retHash["margin-left"] = widget.styles["margin_left"]
-    retHash["padding-top"] = widget.styles["padding_top"]
-    retHash["padding-right"] = widget.styles["padding_right"]
-    retHash["padding-bottom"] = widget.styles["padding_bottom"]
-    retHash["padding-left"] = widget.styles["padding_left"]
+    retHash["color"] = widget.styles["text_color"] if widget.styles["text_color"].present?
+    retHash["margin-top"] = widget.styles["margin_top"] if widget.styles["margin_top"].present?
+    retHash["margin-right"] = widget.styles["margin_right"] if widget.styles["margin_right"].present?
+    retHash["margin-bottom"] = widget.styles["margin_bottom"] if widget.styles["margin_bottom"].present?
+    retHash["margin-left"] = widget.styles["margin_left"] if widget.styles["margin_left"].present?
+    retHash["padding-top"] = widget.styles["padding_top"] if widget.styles["padding_top"].present?
+    retHash["padding-right"] = widget.styles["padding_right"] if widget.styles["padding_right"].present?
+    retHash["padding-bottom"] = widget.styles["padding_bottom"] if widget.styles["padding_bottom"].present?
+    retHash["padding-left"] = widget.styles["padding_left"] if widget.styles["padding_left"].present?
     retHash["border"] = "#{widget.styles["border_size"]} solid #{widget.styles["border_color"]}" if widget.styles["border_size"].present?
     retHash["fixed-position"] = widget.styles["fixed_position"] if widget.styles["fixed_position"].present?
     if widget.styles["shadow"].present?
@@ -65,6 +66,21 @@ class WidgetBaseRenderer < BaseRenderer
       retHash["background-color"]=widget.styles['background_color']
     end
     hashToStyleString retHash
+  end
+
+  def tcc_widget_classes
+    retClasses = []
+    if widget.styles["fly_in"] == "fromleft"
+      retClasses << "tcc_fly_in"
+      retClasses << "tcc_fly_in_from_left"
+    elsif widget.styles["fly_in"] == "fromright"
+      retClasses << "tcc_fly_in"
+      retClasses << "tcc_fly_in_from_right"
+    elsif widget.styles["fly_in"] == "frombottom"
+      retClasses << "tcc_fly_in"
+      retClasses << "tcc_fly_in_from_bottom"
+    end
+    retClasses.join(' ')
   end
 end
 
