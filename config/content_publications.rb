bf20a4470be236b55732b8f9ba21c2f7395c4681
Lee Atchison <lee@leeatchison.com>
2025-04-25 22:06:57 -0700

    Content models and such.

---
+++ config/content_publications.rb
@@ -0,0 +1,24 @@
+#
+# Content
+
+ContentType = %i[
+    article
+    podcast
+    video
+  ].freeze # Add more...
+ContentStatus = %i[
+    draft
+    in_review
+    ready
+  ].freeze
+
+ContentReadMoreMessages = {
+  readmore: "Read More",
+  contread: "Continue Reading",
+  clickcontread: "Click to Continue Reading",
+  readmorehere: "Click to Read More Here",
+  continue: "Click Here to Continue",
+  readrest: "Click To Read The Rest",
+  readhere: "Read the Rest of the Article Here",
+  custom: "<custom message>",
+}
