5adeb3d592744205458f54227f8e3c8b8f01f08a
Lee Atchison <lee@leeatchison.com>
2025-04-29 15:04:28 -0700

    Menus.

---
+++ config/menu_options.rb
@@ -0,0 +1,10 @@
+MenuOptions=[
+  ["Basic, Left Aligned","left"],
+  ["Basic, Right Aligned","right"],
+  ["Vertical Top, Left Aligned","vertical-top-left"],
+  ["Vertical Top, Centered","vertical-top-center"],
+  ["Vertical Top, Right Aligned","vertical-top-right"],
+  ["Vertical Bottom, Left Aligned","vertical-bottom-left"],
+  ["Vertical Bottom, Centered","vertical-bottom-center"],
+  ["Vertical Bottom, Right Aligned","vertical-bottom-right"],
+].freeze
