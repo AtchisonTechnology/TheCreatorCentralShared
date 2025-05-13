0c4744cba6c4bbcc09ce8b625b097aa8f446648c
Lee Atchison <lee@leeatchison.com>
2025-05-13 08:25:59 -0700

    basic themes.

---
+++ themes/javascript/plain/main.js
@@ -0,0 +1,3 @@
+function helloThere(){
+    alert("Hello There!");
+}
---
+++ themes/plain.js
@@ -0,0 +1,23 @@
+// Entry point for the build script in your package.json
+
+//
+// Javascript
+//
+// import FlyIn from './javascript/fly_in.js';
+import './javascript/plain/main.js';
+
+//
+// Widgets (React)
+//
+
+
+//
+// CSS
+//
+import './stylesheets/plain/main.css';
+import './stylesheets/plain/support.css';
+
+//
+// Initialization Code
+//
+// FlyIn.setup();
