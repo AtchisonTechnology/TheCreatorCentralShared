4eea35077ff811a62ba644c5c1e73feed4967a32
Lee Atchison <lee@leeatchison.com>
2025-05-14 10:41:40 -0700

    Themes.

---
+++ themes/theme-sai.js
@@ -0,0 +1,15 @@
+// Theme entry point for JS and CSS support
+
+//
+// CSS
+//
+import './shared/shared.css';
+import './theme-sai.css';
+
+//
+// Javascript
+//
+
+//
+// Initialization Code
+//
---
+++ themes/theme-simplee.js
@@ -0,0 +1,15 @@
+// Theme entry point for JS and CSS support
+
+//
+// CSS
+//
+import './shared/shared.css';
+import './theme-simplee.css';
+
+//
+// Javascript
+//
+
+//
+// Initialization Code
+//
