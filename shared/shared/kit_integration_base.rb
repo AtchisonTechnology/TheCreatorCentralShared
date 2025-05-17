b98352edc77e1f7c49b63643d5104d4129f2b6d2
Lee Atchison <lee@leeatchison.com>
2025-05-17 06:56:07 -0700

    Cleanup s3 nginx files.

--- shared/shared/kit_integration_base.rb
+++ shared/shared/kit_integration_base.rb
@@ -165,7 +165,7 @@ module KitIntegrationBase
     resp = Net::HTTP.start(uri.hostname,uri.port,use_ssl: true) do |http|
       req = Net::HTTP::Post.new(uri)
       req.body= {
-        "client_id": SecretsManager.get_secret('prod/TccSite/Credentials')['kit_client_id'],
+        "client_id": get_kit_client_id,
         "grant_type": "refresh_token",
         "refresh_token": self.identity.refresh_token,
       }.to_json
